465e3264e348535051d5133c9e5e98a7ddba9414
Shay-Delaney-Dev <delaneyshay44@outlook.com>
2026-09-10 19:03:20 +0100

    Fixed issue where information was overlapping into other columns by adding fit() function

--- README.md
+++ README.md
@@ -3,6 +3,5 @@ Simple network scanner that uses nmap to run a scan on a target IP address/hostn
 
 # Note
 I made this basic network scanner using free online resources to help me develop my interest in computer networking and cybersecurity.
-
 The article I used to develop this project:
 https://medium.com/@amaltomparakkaden/automating-network-scanning-with-python-and-nmap-948948f0b161
--- networkScanner.py
+++ networkScanner.py
@@ -1,5 +1,13 @@
 import nmap
-# from pprint import pprint
+
+def fit(value, width):
+    """Fit a value into a fixed-width column. Prevents confusing overlapping of information spilling into other columns"""
+    value = str(value or "")
+
+    if len(value) > width:
+        return value[:width - 1] + "…"
+
+    return value.ljust(width)
 
 # Create new scanner object
 scanner = nmap.PortScanner()
@@ -16,37 +24,35 @@ scanner.scan(target, arguments=options)
 print("\n----- Scan Results -----\n")
 
 for host in scanner.all_hosts():
-    # pprint(scanner[host]) -- shows all data -- 
     print(f"Host: {host}")
     print(f"State: {scanner[host].state()}\n")
 
     for proto in scanner[host].all_protocols():
         print(f"Protocol: {proto.upper()}")
 
-        # Table header
         print(
-            f"{'PORT':<8}"
-            f"{'STATE':<10}"
-            f"{'REASON':<12}"
-            f"{'SERVICE':<15}"
-            f"{'PRODUCT':<22}"
-            f"{'VERSION':<15}"
-            f"{'EXTRA INFO':<25}"
+            fit("PORT", 8),
+            fit("STATE", 10),
+            fit("REASON", 12),
+            fit("SERVICE", 15),
+            fit("PRODUCT", 20),
+            fit("VERSION", 20),
+            fit("EXTRA INFO", 30)
         )
 
-        print("-" * 107)
+        print("-" * 127)
 
         for port in sorted(scanner[host][proto]):
             info = scanner[host][proto][port]
 
             print(
-                f"{port:<8}"
-                f"{info.get('state', ''):<10}"
-                f"{info.get('reason', ''):<12}"
-                f"{info.get('name', ''):<15}"
-                f"{info.get('product', ''):<22}"
-                f"{info.get('version', ''):<15}"
-                f"{info.get('extrainfo', ''):<25}"
+                fit(port, 8),
+                fit(info.get("state"), 10),
+                fit(info.get("reason"), 12),
+                fit(info.get("name"), 15),
+                fit(info.get("product"), 20),
+                fit(info.get("version"), 20),
+                fit(info.get("extrainfo"), 30)
             )
 
         print()
